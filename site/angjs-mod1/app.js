96097c2962cbcb2538ede116f07c967fe95081ea
Ryan Coleman <rcolem34@jhu.edu>
2022-03-08 15:26:20 -0500

    finished assignment and bonus objectives

--- site/angjs-mod1/app.js
+++ site/angjs-mod1/app.js
@@ -1,4 +1,29 @@
 (function () {
     'use strict';
     angular.module('LunchCheck', [])
+        .controller("LunchCheckController", LunchCheckController)
+    function LunchCheckController($scope) {
+        $scope.checkIfTooMuch = function () {
+            var foodList = $scope.foodList;
+
+            if (foodList === undefined || foodList == "") {
+                $scope.output = "Please enter data first";
+                $scope.outputClassText = "badInput";
+                $scope.outputClassBorder = "badBorder";
+                return;
+            }
+
+            var foodArray = foodList.split(",");
+            var counter = 0;
+            foodArray.forEach(function (element) {
+                if (element.trim() != "") {
+                    counter++;
+                }
+            })
+            $scope.output = (counter < 4) ? "Enjoy!" : "Too Much!";
+            $scope.outputClassText = "goodInput";
+            $scope.outputClassBorder = "goodBorder";
+        };
+    }
+    LunchCheckController.$inject = ['$scope'];
 })();
