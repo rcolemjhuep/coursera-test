9345e022282afd24599e2b0e243a465f8a02335e
Ryan Coleman <rcolem34@jhu.edu>
2022-03-15 06:30:59 -0400

    in progress

--- site/angjs-mod2/js/app.js
+++ site/angjs-mod2/js/app.js
@@ -5,8 +5,15 @@
         .controller("AlreadyBoughtController", AlreadyBoughtController)
         .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
+    var initialList = [
+        { name: "Cookies", amount: 10, pricePerItem: 0.5 },
+        { name: "Soda", amount: 5, pricePerItem: 1.0 },
+        { name: "Chips", amount: 6, pricePerItem: 0.75 },
+        { name: "Gulab Jamons", amount: 100, pricePerItem: 0.35 },
+        { name: "Sandwiches", amount: 3, pricePerItem: 3.0 }]
+
     function ShoppingListCheckOffService() {
-        var toBuyItems = [{ name: "Cookies", amount: 10 }, { name: "Soda", amount: 5 }];
+        var toBuyItems = initialList;
         var holdingItems = [];
 
         this.getToBuyItems = function () {
