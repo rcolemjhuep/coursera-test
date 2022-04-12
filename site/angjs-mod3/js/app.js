e7a5f5b385b71ab7d7b19749e5c5c206df960475
Ryan Coleman <rcolem34@jhu.edu>
2022-04-12 17:45:04 -0400

    Code cleanup

--- site/angjs-mod3/js/app.js
+++ site/angjs-mod3/js/app.js
@@ -23,73 +23,66 @@
     }
 
 
+    // Empty
     function NarrowItDownDirectiveController() {
-        var list = this;
-
-        list.cookiesInList = function () {
-            for (var i = 0; i < list.items.length; i++) {
-                var name = list.items[i].name;
-                if (name.toLowerCase().indexOf("cookie") !== -1) {
-                    return true;
-                }
-            }
-
-            return false;
-        };
     }
-
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     function NarrowItDownController(MenuSearchService) {
         var narrowList = this;
 
+        // Description of item we are looking for
         narrowList.searchTerm = "";
 
+        // Initially grab all menu items possible
         var promise = MenuSearchService.getMenuItems()
         promise.then(function (items) {
-            narrowList.found = items
+            narrowList.found = items;
         })
 
 
+        // Remove item by index
         narrowList.removeItem = function (itemIndex) {
             narrowList.found.splice(itemIndex, 1);
         };
 
+        // Search bar function for finding our search term
         narrowList.narrowFoundList = function () {
 
             // Don't do anything on empty search box
             if (narrowList.searchTerm === undefined || narrowList.searchTerm == "") {
                 MenuSearchService.getMenuItems().then(function (items) {
-                    narrowList.found = items
-                })
-            };
+                    narrowList.found = items;
+                });
+            }
 
             MenuSearchService.getMatchedMenuItems(narrowList.searchTerm).then(function (items) {
-                narrowList.found = items
-            })
+                narrowList.found = items;
+            });
         }
-
-
     }
 
     MenuSearchService.$inject = ['$http']
     function MenuSearchService($http) {
-        var service = this
+        var service = this;
 
+        // Get all menu items available
         service.getMenuItems = function () {
             return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json').then(function (result) {
-                return result.data.menu_items
+                return result.data.menu_items;
             });
         }
 
+
+        // Get all menu items where name or description match search term
         service.getMatchedMenuItems = function (searchTerm) {
             return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json').then(function (result) {
                 // process result and only keep items that match
                 return result.data.menu_items.filter(function (item) {
-                    var lowerDescription = item.description.toLowerCase()
-                    var lowerName = item.name.toLowerCase()
-                    var lowerSearch = searchTerm.toLowerCase()
-                    return lowerDescription.includes(lowerSearch) || lowerName.includes(lowerSearch)
+                    var lowerDescription = item.description.toLowerCase();
+                    var lowerName = item.name.toLowerCase();
+                    var lowerSearch = searchTerm.toLowerCase();
+                    return lowerDescription.includes(lowerSearch) || lowerName.includes(lowerSearch);
                 })
             });
         }
