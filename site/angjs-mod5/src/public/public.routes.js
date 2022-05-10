8fa656fb0cb6321589c2afd2c7f5bf6019488b08
Ryan Coleman <rcolem34@jhu.edu>
2022-05-10 03:44:19 -0400

    basic signup form done

--- site/angjs-mod5/src/public/public.routes.js
+++ site/angjs-mod5/src/public/public.routes.js
@@ -1,67 +1,62 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('public')
-.config(routeConfig);
+  angular.module('public')
+    .config(routeConfig);
 
-/**
- * Configures the routes and views
- */
-routeConfig.$inject = ['$stateProvider'];
-function routeConfig ($stateProvider) {
-  // Routes
-  $stateProvider
-    .state('public', {
-      abstract: true,
-      templateUrl: 'src/public/public.html'
-    })
-    .state('public.home', {
-      url: '/',
-      templateUrl: 'src/public/home/home.html'
-    })
-    .state('public.menu', {
-      url: '/menu',
-      templateUrl: 'src/public/menu/menu.html',
-      controller: 'MenuController',
-      controllerAs: 'menuCtrl',
-      resolve: {
-        menuCategories: ['MenuService', function (MenuService) {
-          return MenuService.getCategories();
-        }]
-      }
-    })
-    .state('public.signup', {
-      url: '/signup',
-      templateUrl: 'src/public/menu/menu.html',
-      controller: 'MenuController',
-      controllerAs: 'signupCtrl',
-      resolve: {
-        menuCategories: ['MenuService', function (MenuService) {
-          return MenuService.getCategories();
-        }]
-      }
-    })
-    .state('public.myinfo', {
-      url: '/myinfo',
-      templateUrl: 'src/public/menu/menu.html',
-      controller: 'MenuController',
-      controllerAs: 'myinfoCtrl',
-      resolve: {
-        menuCategories: ['MenuService', function (MenuService) {
-          return MenuService.getCategories();
-        }]
-      }
-    })
-    .state('public.menuitems', {
-      url: '/menu/{category}',
-      templateUrl: 'src/public/menu-items/menu-items.html',
-      controller: 'MenuItemsController',
-      controllerAs: 'menuItemsCtrl',
-      resolve: {
-        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
-          return MenuService.getMenuItems($stateParams.category);
-        }]
-      }
-    });
-}
+  /**
+   * Configures the routes and views
+   */
+  routeConfig.$inject = ['$stateProvider'];
+  function routeConfig($stateProvider) {
+    // Routes
+    $stateProvider
+      .state('public', {
+        abstract: true,
+        templateUrl: 'src/public/public.html'
+      })
+      .state('public.home', {
+        url: '/',
+        templateUrl: 'src/public/home/home.html'
+      })
+      .state('public.menu', {
+        url: '/menu',
+        templateUrl: 'src/public/menu/menu.html',
+        controller: 'MenuController',
+        controllerAs: 'menuCtrl',
+        resolve: {
+          menuCategories: ['MenuService', function (MenuService) {
+            return MenuService.getCategories();
+          }]
+        }
+      })
+      .state('public.signup', {
+        url: '/signup',
+        templateUrl: 'src/public/signup/signup.html',
+        controller: 'SignUpController',
+        controllerAs: 'signupCtrl',
+      })
+      .state('public.myinfo', {
+        url: '/myinfo',
+        templateUrl: 'src/public/menu/menu.html',
+        controller: 'MenuController',
+        controllerAs: 'myinfoCtrl',
+        resolve: {
+          menuCategories: ['MenuService', function (MenuService) {
+            return MenuService.getCategories();
+          }]
+        }
+      })
+      .state('public.menuitems', {
+        url: '/menu/{category}',
+        templateUrl: 'src/public/menu-items/menu-items.html',
+        controller: 'MenuItemsController',
+        controllerAs: 'menuItemsCtrl',
+        resolve: {
+          menuItems: ['$stateParams', 'MenuService', function ($stateParams, MenuService) {
+            return MenuService.getMenuItems($stateParams.category);
+          }]
+        }
+      });
+  }
 })();
---
+++ site/angjs-mod5/src/public/signup/signup.controller.js
@@ -0,0 +1,17 @@
+(function () {
+  "use strict";
+
+  angular.module('public')
+    .controller('SignUpController', SignUpController);
+
+  // SignUpController.$inject = [''];
+  function SignUpController() {
+    var $ctrl = this;
+
+    $ctrl.submit = function () {
+
+    }
+  }
+
+
+})();
